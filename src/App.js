a09957ca1fa4a0bc5d940660fbe467cf4287bc60
Julian Krispel-Samsel <juliankrispel@yahoo.de>
2017-06-19 09:25:24 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { EditorState, Editor } from 'draft-js';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorState: EditorState.createEmpty(),
+    }
+  }
+
+  onChange = (editorState) => {
+    this.setState({
+      editorState,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Editor
+          editorState={this.state.editorState}
+          onChange={this.onChange}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
