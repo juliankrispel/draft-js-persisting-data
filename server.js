b4c11fee2a7eeaa52d0c210d4aa4c7a918947b72
Julian Krispel-Samsel <juliankrispel@yahoo.de>
2017-06-20 10:28:12 +0100

    added mini-server

---
+++ server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import storage from 'node-persist';
+
+storage.init({
+  dir: 'data',
+  stringify: JSON.stringify,
+  parse: JSON.parse,
+  encoding: 'utf8',
+}).then(() => {
+  console.log('storage initialized');
+}).catch((e) => {
+  console.log('storage initialization failed');
+  console.error(e);
+});
+
+const app = express();
+
+app.use(bodyParser.json())
+
+app.get('/content', (req, res) => {
+  storage.getItem('content', (err, val) => {
+    res.json(val || {});
+  });
+});
+
+app.post('/content', (req, res) => {
+  storage.setItem('content', req.body.content, (err, val) => {
+    res.json({ok: true});
+  });
+});
+
+app.listen(3001);
+console.log('listening on http://localhost:3001');
